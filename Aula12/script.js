052789f98c99f0e7438c24d84be7db53c52065f6
clarasousamartins <clarasousam6@gmail.com>
2023-09-29 16:48:54 -0300

    Tabuada

--- Aula12/script.js
+++ Aula12/script.js
@@ -1,47 +1,12 @@
-// máscara para o cpf
-let input = document.querySelector ("#cpf")
-//qurySelector:tag, id, classe
-//para id e classe utilizar a mesma notação usada pelo css
-//("input")
-
-// () função anônima
-// => arrow function
-// function nome ()
-//{ }
-
-input.addEventListener("keypress", () => {
-let inputLenght= input.value.length
-
-if (inputLenght==3 || inputLenght==7 ) {
-    input.value+= "."
-    //imput.value = input.value+"."
-    }
-
-else if (inputLenght==11){
-        input.value+= "."
-}
-
-})
-
-//=======================================================
-//máscara para o cnpj
-let inputCnpj=document.querySelector ("#cnpj")
-inputCnpj.addEventListener("keypress",() =>
+function tabuada()
 {
-    let cnpjLength=inputCnpj.value.length
-    if(cnpjLength==2 ||cnpjLength==6 )
-    {
-    inputCnpj.value+="."
-    }
-    if(cnpjLength==10)
-    {
-    inputCnpj.value+="/"
-    }
-    if(cnpjLength==15)
-    {
-    inputCnpj.value+="-"
-    }
-})
-
-
-
+    var num = parseInt(document.getElementById("num").value);
+    var resposta = document.getElementById('resposta');
+    var tabuada='';
+  
+    for(var count=1; count<=10 ; count++)
+     tabuada += num+" x "+ count+" = "+
+                 num*count+"<br />";
+                 
+    resposta.innerHTML = tabuada;
+}                 
